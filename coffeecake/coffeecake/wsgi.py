28fccb32ccefa9f219ec8e9728b8a5ab29eaff81
Marcos Gabarda <marcosgabarda@gmail.com>
2014-11-05 21:26:00 +0100

    deploy setings

--- coffeecake/coffeecake/wsgi.py
+++ coffeecake/coffeecake/wsgi.py
@@ -10,5 +10,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeecake.settings.local")
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
