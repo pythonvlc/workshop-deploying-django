95f7b38e9dd81b0471df027007f569e40213ffe4
Marcos Gabarda <marcosgabarda@gmail.com>
2014-11-05 21:26:50 +0100

    deploy setings

--- coffeecake/manage.py
+++ coffeecake/manage.py
@@ -5,6 +5,12 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeecake.settings")
 
+    try:
+        import pymysql
+        pymysql.install_as_MySQLdb()
+    except ImportError:
+        pass
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
