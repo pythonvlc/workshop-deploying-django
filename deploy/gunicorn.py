023a0feb8eea486a3b2a4e13e58e7f8f86d87a7f
Marcos Gabarda <marcosgabarda@gmail.com>
2014-11-05 21:15:24 +0100

    deploy setings

--- deploy/gunicorn.py
+++ deploy/gunicorn.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 chdir = "/home/ubuntu/workshop-deploying-django/coffeecake"
 bind = "unix:/home/ubuntu/coffeecake.sock"
+env = 'DJANGO_SETTINGS_MODULE="coffeecake.settings.production"'
 workers = 2
