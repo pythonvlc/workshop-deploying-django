7acb539f9556f12d7024ff4fc703c7e1b6ab3a19
Marcos Gabarda <marcosgabarda@gmail.com>
2014-11-05 21:06:20 +0100

    deploy setings

--- deploy/gunicorn.py
+++ deploy/gunicorn.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
+chdir = "/home/ubuntu/workshop-deploying-django/coffeecake"
 bind = "unix:/home/ubuntu/coffeecake.sock"
 workers = 2
